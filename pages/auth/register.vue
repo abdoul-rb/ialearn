1a363b2ffd782b9c4b67990953ac9a87211c107d
abdoul-rb <abdoulrahim.tools@gmail.com>
2020-12-25 20:53:53 +0100

    custom 2 login page and create logo component

--- pages/auth/register.vue
+++ pages/auth/register.vue
@@ -26,7 +26,7 @@
             </div>
           </div>
           <div>
-            <button type="submit" class="w-full flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">Sign in</button>
+            <button type="submit" class="w-full flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">Inscrivez-vous</button>
           </div>
         </form>
         <div class="mt-6">
@@ -91,7 +91,7 @@
     methods: {
       async submit() {
         try {
-          await this.$axios.post('auth/register', this.form )
+          await this.$axios.post('auth/register', this.form);
           this.$router.push({ name : 'dashboard' })
         } catch (e) {
           console.log(e.response.data)
