e52cd1617b13f9ce0341fdc042694ae1132e7185
abdoul-rb <abdoulrahim.tools@gmail.com>
2021-02-25 17:50:12 +0100

    Add forum page on comming soon.

---
+++ pages/forum/index.vue
@@ -0,0 +1,24 @@
+<template>
+  <main class="min-h-screen">
+    <div class="flex items-center justify-center min-h-screen">
+      <h1 class="text-4xl font-medium tracking-wide">
+        Posez prochainement vos questions ici.
+      </h1>
+    </div>
+  </main>
+</template>
+<script>
+  export default {
+    layout: 'app',
+    head() {
+      return {
+        title: 'Forum de discussion'
+      }
+    },
+    data() {
+      return {
+
+      }
+    },
+  }
+</script>
