cf3ddb17492f5cde37bed97f17379d058c1e8b09
abdoul-rb <abdoulrahim.tools@gmail.com>
2021-04-11 19:23:14 +0200

    refactor(): update and refactor student dashboard overview and courses

--- pages/app/student/dashboard/courses/index.vue
+++ pages/app/student/dashboard/courses/index.vue
@@ -1,64 +1,60 @@
 <template>
-  <main>
-    <header class="flex items-center">
-      <h2 class="text-xl font-semibold tracking-wide">All courses</h2>
-      <dashboard-courses-navigation />
-    </header>
-    <div class="py-12">
-      <div class="grid grid-cols-4 gap-6">
-        <div class="col-span-4 rounded-md shadow bg-indigo-600 transform hover:-translate-x-2 transtion transition-all duration-500 px-8 py-6">
-          <div class="flex items-baseline justify-between text-white">
-            <h4 class="text-2xl font-medium">UI/UX Design</h4>
-            <h5 class="text-md font-medium opacity-75">3h45min</h5>
-          </div>
-          <h5 class="text-md text-white font-medium opacity-75">7 chapitres</h5>
-          <div class="flex items-center justify-between mt-24">
-            <button class="bg-gray-100 opacity-50 w-10 h-10 rounded flex items-center justify-center" title="Continuez le cours">
-              <svg class="stroke-current text-gray-800 w-5 h-5" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
-                <polygon points="5 3 19 12 5 21 5 3"></polygon>
+  <div class="font-nunito">
+    <div>
+      <h3 class="text-xl leading-6 font-semibold tracking-wide text-gray-900">Mes cours</h3>
+      <div class="mt-5 grid grid-cols-1 gap-5 sm:grid-cols-2">
+        <div v-for="x in 5" class="px-4 py-5 bg-white shadow rounded-lg overflow-hidden sm:px-6 sm:py-4 transform hover:-translate-x-2 transition transition-all duration-500">
+          <div class="flex items-baseline justify-between">
+            <h4 class="text-lg font-semibold leading-9 text-gray-700 w-2/3">Product Analytics</h4>
+            <nuxt-link :to="{ name: '' }" class="inline-flex p-2 border rounded-md flex items-center justify-center" title="Continuez le cours">
+              <svg class="stroke-current text-gray-500 w-4 h-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
+                <line x1="5" y1="12" x2="19" y2="12"></line><polyline points="12 5 19 12 12 19"></polyline>
               </svg>
-            </button>
+            </nuxt-link>
           </div>
-        </div>
-        <div class="col-span-2 rounded-md shadow bg-green-500 px-8 py-6">
-          <div class="flex items-baseline justify-between text-white">
-            <h4 class="text-2xl font-medium">Power BI</h4>
-            <h5 class="text-md font-medium opacity-75">7h</h5>
-          </div>
-          <h5 class="text-md text-white font-medium opacity-75">12 chapitres</h5>
-          <div class="flex items-center justify-between mt-24">
-            <button class="bg-gray-100 opacity-50 w-10 h-10 rounded flex items-center justify-center" title="Continuez le cours">
-              <svg class="stroke-current text-gray-800 w-5 h-5" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
-                <polygon points="5 3 19 12 5 21 5 3"></polygon>
+          <div class="flex items-center space-x-4">
+            <span class="inline-flex items-center text-sm font-medium">
+              <svg class="-mt-0.5 mr-1 h-4 w-4 text-gray-500" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
+                <path d="M14 2H6a2 2 0 0 0-2 2v16a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V8z"></path><polyline points="14 2 14 8 20 8"></polyline><line x1="16" y1="13" x2="8" y2="13"></line><line x1="16" y1="17" x2="8" y2="17"></line><polyline points="10 9 9 9 8 9"></polyline>
               </svg>
-            </button>
-          </div>
-        </div>
-        <div class="col-span-2 rounded-md shadow bg-yellow-600 px-8 py-6">
-          <div class="flex items-baseline justify-between text-white">
-            <h4 class="text-2xl font-medium">L'apprentissage</h4>
-            <h5 class="text-md font-medium opacity-75">2h</h5>
-          </div>
-          <h5 class="text-md text-white font-medium opacity-75">3 chapitres</h5>
-          <div class="flex items-center justify-between mt-24">
-            <button class="bg-gray-100 opacity-50 w-10 h-10 rounded flex items-center justify-center" title="Continuez le cours">
-              <svg class="stroke-current text-gray-800 w-5 h-5" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
-                <polygon points="5 3 19 12 5 21 5 3"></polygon>
+              4 Chapitres
+            </span>
+            <span class="inline-flex items-center text-sm font-medium">
+              <svg class="-mt-0.5 mr-1 h-4 w-4 text-gray-500" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
+                <circle cx="12" cy="12" r="10"></circle><polyline points="12 6 12 12 16 14"></polyline>
               </svg>
-            </button>
+              3h 55
+            </span>
+          </div>
+          <div class="mt-20">
+            <div class="flex items-center justify-between">
+              <span class="inline-flex items-center text-sm font-medium">
+                <svg class="-mt-0.5 mr-1 h-3 w-3 text-cyan-600" fill="currentColor" viewBox="0 0 8 8">
+                  <circle cx="4" cy="4" r="3" />
+                </svg>
+                En cours
+              </span>
+              <div class="text-sm font-semibold">64%</div>
+            </div>
+            <div class="relative mt-2">
+              <div class="overflow-hidden h-1.5 mb-4 text-xs flex rounded bg-cyan-100">
+                <div style="width:30%" class="shadow-none flex flex-col text-center whitespace-nowrap text-white justify-center bg-cyan-600"></div>
+              </div>
+            </div>
           </div>
         </div>
       </div>
     </div>
-  </main>
+  </div>
 </template>
 
 <script>
   export default {
     layout: 'student',
+    middleware: 'auth',
     head () {
       return {
-        title: 'Tableau de bord - Mes cours'
+        title: 'Mes cours'
       }
     }
   }
--- pages/app/student/dashboard/index.vue
+++ pages/app/student/dashboard/index.vue
@@ -1,7 +1,97 @@
 <template>
-  <div>
-    <div class="flex items-center justify-center h-96">
-      <h1 class="text-5xl font-nunito font-semibold opacity-25">Overview is Comming soon ...</h1>
+  <div class="font-nunito">
+    <div>
+      <h3 class="text-xl leading-6 font-semibold tracking-wide text-gray-900">Statistiques</h3>
+      <dl class="mt-5 grid grid-cols-1 gap-5 sm:grid-cols-3">
+        <div class="px-4 py-5 bg-white shadow rounded-lg overflow-hidden sm:p-6">
+          <dt class="mt-1 text-3xl font-semibold text-gray-900">
+            3
+          </dt>
+          <dd class="text-base font-medium text-gray-700 truncate">
+            Cours complétés
+          </dd>
+        </div>
+
+        <div class="px-4 py-5 bg-white shadow rounded-lg overflow-hidden sm:p-6">
+          <dt class="mt-1 text-3xl font-semibold text-gray-900">
+            4
+          </dt>
+          <dd class="text-base font-medium text-gray-700 truncate">
+            Cours en cours
+          </dd>
+        </div>
+
+        <div class="px-4 py-5 bg-white shadow rounded-lg overflow-hidden sm:p-6">
+          <dt class="mt-1 text-3xl font-semibold text-gray-900">
+            20.6
+          </dt>
+          <dd class="text-base font-medium text-gray-700 truncate">
+            Heures de cours suivies
+          </dd>
+        </div>
+      </dl>
+    </div>
+    <div class="mt-12">
+      <div class="flex items-center justify-between">
+        <h3 class="text-xl leading-6 font-semibold tracking-wide text-gray-900">Mes derniers cours</h3>
+        <nuxt-link :to="{ name: '' }" class="text-sm font-semibold text-indigo-600 px-2 py-1">Voir tous</nuxt-link>
+      </div>
+      <div class="flex flex-col mt-6">
+        <div class="-my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
+          <div class="py-2 align-middle inline-block min-w-full sm:px-6 lg:px-8">
+            <div class="overflow-hidden sm:rounded-sm">
+              <table class="min-w-full">
+                <thead class="bg-white">
+                  <tr>
+                    <th scope="col" class="px-6 py-3 text-left text-sm font-semibold text-gray-500 tracking-wider">
+                      Cours
+                    </th>
+                    <th scope="col" class="px-6 py-3 text-left text-sm font-semibold text-gray-500 tracking-wider">
+                      Debut
+                    </th>
+                    <th scope="col" class="px-6 py-3 text-left text-sm font-semibold text-gray-500 tracking-wider">
+                      Type
+                    </th>
+                    <th scope="col" class="relative px-6 py-3">
+                      <span class="sr-only">Favoris</span>
+                    </th>
+                  </tr>
+                </thead>
+                <tbody class="">
+                  <tr v-for="x in 3">
+                    <td class="px-6 py-4 whitespace-nowrap">
+                      <div class="flex items-center">
+                        <div class="flex-shrink-0 h-10 w-10">
+                          <img class="h-10 w-10 rounded-lg" src="https://images.unsplash.com/photo-1475669698648-2f144fcaaeb1?ixid=MXwxMjA3fDB8MHxzZWFyY2h8MTR8fGRlc2lnbnxlbnwwfHwwfA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=60" alt="">
+                        </div>
+                        <div class="ml-4 leading-loose">
+                          <div class="text-sm font-semibold text-gray-900">
+                            Basic of Mobile UX
+                          </div>
+                          <div class="text-sm text-gray-500">
+                            By iaLearn
+                          </div>
+                        </div>
+                      </div>
+                    </td>
+                    <td class="px-6 py-4 whitespace-nowrap">
+                      <div class="text-sm text-gray-900">Janvier 2021</div>
+                    </td>
+                    <td class="px-6 py-4 whitespace-nowrap">
+                      <span class="px-3 py-0.5 inline-flex text-xs leading-5 font-semibold rounded-md bg-gray-300 text-gray-900">
+                        Design
+                      </span>
+                    </td>
+                    <td class="px-6 py-4 whitespace-nowrap text-right text-sm font-medium">
+                      <nuxt-link :to="{ name: '' }" class="text-indigo-600 hover:text-indigo-900">Lire</nuxt-link>
+                    </td>
+                  </tr>
+                </tbody>
+              </table>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   </div>
 </template>
@@ -9,9 +99,9 @@
 <script>
   export default {
     layout: 'student',
+    middleware: 'auth',
     head () {
       return {
-
       }
     }
   }
