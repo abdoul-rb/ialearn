b7d597a0179f02ec72550cd97d9c13669b43518b
abdoul-rb <abdoulrahimbah6@gmail.com>
2024-02-11 21:52:18 +0100

    Update homepage

--- pages/index.vue
+++ pages/index.vue
@@ -21,25 +21,22 @@
 							</button>
 						</div>
 						
-						<dl class="mt-8 grid grid-cols-1 gap-5 sm:grid-cols-3">
+						<dl class="mt-8 grid grid-cols-1 gap-5 sm:grid-cols-3 font-nunito">
 							<div class="overflow-hidden p-3 group hover:bg-gray-200 rounded-xl transition-all">
-								<dd class="text-3xl font-semibold tracking-tight text-green-900 italic">
-									<!-- <svg class="flex-shrink-0 w-4 h-4 text-gray-500" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
-										<circle cx="12" cy="12" r="10"/><path d="M9.09 9a3 3 0 0 1 5.83 1c0 2-3 3-3 3"/><path d="M12 17h.01"/>
-									</svg> -->
+								<dd class="text-3xl font-bold tracking-tight text-green-900 italic">
 									71M
 								</dd>
-								<dt class="mt-1 truncate text-sm font-nunito font-semibold text-black tracking-wide">Universités</dt>
+								<dt class="mt-1 truncate text-sm font-semibold text-black tracking-wide">Universités</dt>
 							</div>
 
 							<div class="overflow-hidden p-3 group hover:bg-gray-200 rounded-xl transition-all">
-								<dd class="text-3xl font-semibold tracking-tight text-green-900 italic">102M</dd>
-								<dt class="mt-1 truncate text-sm font-nunito font-semibold text-black tracking-wide">Étudiants</dt>
+								<dd class="text-3xl font-bold tracking-tight text-green-900 italic">102M</dd>
+								<dt class="mt-1 truncate text-sm font-semibold text-black tracking-wide">Étudiants</dt>
 							</div>
 
 							<div class="overflow-hidden p-3 group hover:bg-gray-200 rounded-xl transition-all">
-								<dd class="text-3xl font-semibold tracking-tight text-green-900 italic">56M</dd>
-								<dt class="mt-1 truncate text-sm font-nunito font-semibold text-black tracking-wide">Formations</dt>
+								<dd class="text-3xl font-bold tracking-tight text-green-900 italic">56M</dd>
+								<dt class="mt-1 truncate text-sm font-semibold text-black tracking-wide">Formations</dt>
 							</div>
 						</dl>
 					</div>
@@ -66,203 +63,70 @@
 	</div>
 
 	<!-- Card Blog -->
-	<section class="bg-gray-50 my-8">
+	<section class="bg-gray-50 my-8 font-nunito ">
 		<div class="max-w-7xl mx-auto px-4 py-10 sm:px-6 lg:px-0 lg:py-14">
 			<div class="max-w-2xl mx-auto text-center mb-10 lg:mb-14">
 				<h2 class="text-2xl font-semibold md:text-3xl md:leading-tight">Nos dernières formations</h2>
-				<p class="mt-1 text-gray-900">See how game-changing companies are making the most of every.</p>
+				<p class="mt-3 text-lg text-gray-600 leading-5">
+					<span class="font-medium">ialearn</span> vous aide à approfondir et sauvegarder vos connaissances. Apprenez et travaillez ensemble à tout moment et en tout lieu.
+				</p>
 			</div>
 
 			<!-- Grid -->
 			<div class="grid sm:grid-cols-2 lg:grid-cols-3 gap-6">
-				<!-- Card -->
-				<a class="group flex flex-col h-full border border-gray-200 hover:border-transparent hover:shadow-lg transition-all duration-300 rounded-xl p-5      /[.4]      " href="#">
-				<div class="aspect-w-16 aspect-h-11">
-					<img class="w-full object-cover rounded-xl" src="https://images.unsplash.com/photo-1633114128174-2f8aa49759b0?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80" alt="Image Description">
-				</div>
-				<div class="my-6">
-					<h3 class="text-xl font-semibold text-gray-800    ">
-					Announcing a free plan for small teams
-					</h3>
-					<p class="mt-5 text-gray-600  ">
-					At Wake, our mission has always been focused on bringing openness.
-					</p>
-				</div>
-				<div class="mt-auto flex items-center gap-x-3">
-					<img class="w-8 h-8 rounded-full" src="https://images.unsplash.com/photo-1438761681033-6461ffad8d80?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=facearea&facepad=2&w=320&h=320&q=80" alt="Image Description">
-					<div>
-					<h5 class="text-sm text-gray-800  ">By Lauren Waller</h5>
-					</div>
-				</div>
-				</a>
-				<!-- End Card -->
+				<template v-for="x in 3" >
+					<nuxt-link :to="{ name: '' }" :key="x" class="group flex flex-col h-full border border-gray-200 hover:shadow-sm transition-all duration-300 rounded-xl p-5">
+						<div class="aspect-w-16 aspect-h-11">
+							<img class="w-full object-cover rounded-xl" src="https://images.unsplash.com/photo-1633114128174-2f8aa49759b0?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80" alt="Image Description">
+						</div>
+						<div class="my-6">
+							<div class="flex items-center gap-x-3">
+								<div class="flex justify-center items-center w-12 h-12 p-3 bg-blue-100 rounded-lg">
+									<svg class="flex-shrink-0 w-5 h-5 text-green-900" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
+										<rect width="10" height="14" x="3" y="8" rx="2"></rect><path d="M5 4a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v16a2 2 0 0 1-2 2h-2.4"></path><path d="M8 18h.01"></path>
+									</svg>
+								</div>
+								<h3 class="text-lg font-semibold text-black">
+									Tout savoir sur Python
+								</h3>
+							</div>
+							
+							<p class="mt-5 text-sm text-gray-900">
+								At Wake, our mission has always been focused on bringing openness.
+							</p>
+						</div>
+						<div class="mt-auto flex items-center justify-between border-t border-gray-200 py-5">
+							<div class="flex items-center gap-x-4">
+								<div class="text-sm font-nunito font-semibold text-black tracking-wide">
+									<span class="text-indigo-600 text-base">4</span> lessons
+								</div>
 
-				<!-- Card -->
-				<a class="group flex flex-col h-full border border-gray-200 hover:border-transparent hover:shadow-lg transition-all duration-300 rounded-xl p-5      /[.4]      " href="#">
-				<div class="aspect-w-16 aspect-h-11">
-					<img class="w-full object-cover rounded-xl" src="https://images.unsplash.com/photo-1562851529-c370841f6536?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=3540&q=80" alt="Image Description">
-				</div>
-				<div class="my-6">
-					<h3 class="text-xl font-semibold text-gray-800    ">
-					How Google Assistant now helps you record stories for kids
-					</h3>
-					<p class="mt-5 text-gray-600  ">
-					Google is constantly updating its consumer AI, Google Assistant, with new features.
-					</p>
-				</div>
-				<div class="mt-auto flex items-center gap-x-3">
-					<img class="w-8 h-8 rounded-full" src="https://images.unsplash.com/photo-1492562080023-ab3db95bfbce?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=facearea&facepad=2&w=320&h=320&q=80" alt="Image Description">
-					<div>
-					<h5 class="text-sm text-gray-800  ">By Aaron Larsson</h5>
-					</div>
-				</div>
-				</a>
-				<!-- End Card -->
+								<div class="text-sm font-nunito font-semibold text-black tracking-wide">
+									<span class="text-indigo-600 text-base">12</span> heures
+								</div>
+							</div>
 
-				<!-- Card -->
-				<a class="group flex flex-col h-full border border-gray-200 hover:border-transparent hover:shadow-lg transition-all duration-300 rounded-xl p-5      /[.4]      " href="#">
-				<div class="aspect-w-16 aspect-h-11">
-					<img class="w-full object-cover rounded-xl" src="https://images.unsplash.com/photo-1521321205814-9d673c65c167?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=3548&q=80" alt="Image Description">
-				</div>
-				<div class="my-6">
-					<h3 class="text-xl font-semibold text-gray-800    ">
-					Front accounts - let's work together
-					</h3>
-					<p class="mt-5 text-gray-600  ">
-					Are you an accountant? Are you a company formation advisor?
-					</p>
-				</div>
-				<div class="mt-auto flex items-center gap-x-3">
-					<img class="w-8 h-8 rounded-full" src="https://images.unsplash.com/photo-1438761681033-6461ffad8d80?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=facearea&facepad=2&w=320&h=320&q=80" alt="Image Description">
-					<div>
-					<h5 class="text-sm text-gray-800  ">By Lauren Waller</h5>
-					</div>
-				</div>
-				</a>
-				<!-- End Card -->
+							<button class="w-full px-4 py-1.5 border border-transparent text-sm font-medium rounded-md text-white bg-gray-800 shadow-sm hover:bg-gray-900 focus:outline-none focus:ring-0 sm:flex-shrink-0 sm:inline-flex sm:items-center sm:w-auto">
+								Commencer
+							</button>
+						</div>
+					</nuxt-link>
+				</template>
 			</div>
 			<!-- End Grid -->
 
-			<!-- Card -->
 			<div class="mt-12 text-center">
-				<a class="py-3 px-4 inline-flex items-center gap-x-1 text-sm font-medium rounded-full border border-gray-200 bg-white text-blue-600 shadow-sm hover:bg-gray-50 disabled:opacity-50 disabled:pointer-events-none              " href="#">
-				Read more
-				<svg class="flex-shrink-0 w-4 h-4" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="m9 18 6-6-6-6"/></svg>
+				<a  href="#" class="py-2.5 px-8 inline-flex items-center gap-x-1 text-sm font-medium rounded-full border border-gray-200 bg-white text-blue-600 shadow-sm hover:bg-gray-50 disabled:opacity-50 disabled:pointer-events-none">
+					Voir les autres cours
+					<svg class="flex-shrink-0 w-4 h-4" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
+						<path d="m9 18 6-6-6-6"/>
+					</svg>
 				</a>
 			</div>
-			<!-- End Card -->
 		</div>
 	</section>
 	<!-- End Card Blog -->
 
-    <section class="sm:py-20">
-      <div class="max-w-7xl mx-auto px-4 md:px-0 sm:mb-12">
-        <h2 class="text-3xl text-gray-800 text-center font-nunito font-extrabold md:text-5xl">
-          Featured
-        </h2>
-        <p class="mt-4 max-w-prose mx-auto text-xl text-gray-500 text-center">
-          Notre plateforme de cours vous permet de bénéficier des derniers tendances en termes d'algorithme et de technologies.
-        </p>
-
-        <div class="mt-16">
-          <div class="grid grid-cols-1 gap-8 sm:grid-cols-2 lg:grid-cols-3">
-            <div class="pt-6">
-              <div class="flow-root bg-gray-50 rounded-lg px-6 pb-8">
-                <div class="-mt-6">
-                  <div class="flex justify-center z-20">
-                      <span class="inline-flex items-center justify-center px-5 py-4 bg-cyan-400 rounded-md shadow-lg">
-                        <!-- Heroicon name: outline/cloud-upload -->
-                        <svg class="h-8 w-8 text-white" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
-                          <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M7 16a4 4 0 01-.88-7.903A5 5 0 1115.9 6L16 6a5 5 0 011 9.9M15 13l-3-3m0 0l-3 3m3-3v12" />
-                        </svg>
-                      </span>
-                  </div>
-                  <div class="text-center">
-                    <h3 class="mt-6 text-4xl font-medium text-black tracking-tight">76</h3>
-                    <h4 class="mt-1 text-xl font-nunito uppercase font-semibold text-gray-800 tracking-wide">Universités</h4>
-                    <p class="mt-3 text-base text-gray-500">
-                      Ac tincidunt sapien vehicula erat auctor pellentesque rhoncus. Et magna sit morbi vitae lobortis.
-                    </p>
-                  </div>
-                </div>
-              </div>
-            </div>
-
-            <div class="pt-6">
-              <div class="flow-root bg-gray-50 rounded-lg px-6 pb-8">
-                <div class="-mt-6">
-                  <div class="flex justify-center z-20">
-                      <span class="inline-flex items-center justify-center px-5 py-4 bg-cyan-400 rounded-md shadow-lg">
-                        <!-- Heroicon name: outline/cloud-upload -->
-                        <svg class="h-8 w-8 text-white" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
-                          <path d="M17 21v-2a4 4 0 0 0-4-4H5a4 4 0 0 0-4 4v2"></path><circle cx="9" cy="7" r="4"></circle><path d="M23 21v-2a4 4 0 0 0-3-3.87"></path><path d="M16 3.13a4 4 0 0 1 0 7.75"></path>
-                        </svg>
-                      </span>
-                  </div>
-                  <div class="text-center">
-                    <h3 class="mt-6 text-4xl font-medium text-black tracking-tight">783</h3>
-                    <h4 class="mt-1 text-xl font-nunito uppercase font-semibold text-gray-800 tracking-wide">Etudiants</h4>
-                    <p class="mt-3 text-base text-gray-500">
-                      Ac tincidunt sapien vehicula erat auctor pellentesque rhoncus. Et magna sit morbi vitae lobortis.
-                    </p>
-                  </div>
-                </div>
-              </div>
-            </div>
-
-            <div class="pt-6">
-              <div class="flow-root bg-gray-50 rounded-lg px-6 pb-8">
-                <div class="-mt-6">
-                  <div class="flex justify-center z-20">
-                      <span class="inline-flex items-center justify-center px-5 py-4 bg-cyan-400 rounded-md shadow-lg">
-                        <!-- Heroicon name: outline/cloud-upload -->
-                        <svg class="h-8 w-8 text-white" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
-                          <rect x="2" y="3" width="20" height="14" rx="2" ry="2"></rect><line x1="8" y1="21" x2="16" y2="21"></line><line x1="12" y1="17" x2="12" y2="21"></line></svg>
-                      </span>
-                  </div>
-                  <div class="text-center">
-                    <h3 class="mt-6 text-4xl font-medium text-black tracking-tight">32</h3>
-                    <h4 class="mt-1 text-xl font-nunito uppercase font-semibold text-gray-800 tracking-wide">Cours</h4>
-                    <p class="mt-3 text-base text-gray-500">
-                      Ac tincidunt sapien vehicula erat auctor pellentesque rhoncus. Et magna sit morbi vitae lobortis.
-                    </p>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-    </section>
-
-    <section class="relative bg-gray-900">
-      <div class="relative h-56 bg-indigo-600 sm:h-72 md:absolute md:left-0 md:h-full md:w-1/2">
-        <img class="w-full h-full object-cover" src="https://images.unsplash.com/photo-1525130413817-d45c1d127c42?ixlib=rb-1.2.1&amp;ixqx=LRDGaNTXAY&amp;ixid=eyJhcHBfaWQiOjEyMDd9&amp;auto=format&amp;fit=crop&amp;w=1920&amp;q=60&amp;sat=-100" alt="">
-        <div aria-hidden="true" class="absolute inset-0 bg-gradient-to-r from-teal-500 to-cyan-600" style="mix-blend-mode: multiply;"></div>
-      </div>
-      <div class="relative mx-auto max-w-md px-4 py-12 sm:max-w-7xl sm:px-6 sm:py-20 md:py-28 lg:px-8 lg:py-32">
-        <div class="md:ml-auto md:w-1/2 md:pl-10">
-          <p class="text-base font-semibold uppercase tracking-wider text-gray-300">Award winning support</p>
-          <h2 class=" text-white font-nunito text-3xl font-extrabold tracking-tight sm:text-4xl">
-            We’re here to help
-          </h2>
-          <p class="mt-3 text-lg text-gray-300">
-            <span class="font-nunito font-medium">ialearn</span> vous aide à approfondir et sauvegarder vos connaissances. Apprenez et travaillez ensemble à tout moment et en tout lieu.
-          </p>
-          <div class="mt-8">
-            <div class="inline-flex rounded-md shadow">
-              <nuxt-link :to="{ name: '' }" class="inline-flex items-center justify-center px-5 py-3 border border-transparent text-base font-medium rounded-md text-gray-900 bg-white hover:bg-gray-50">
-                Visit the help center
-                <svg class="-mr-1 ml-3 h-5 w-5 text-gray-400" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
-                  <path d="M11 3a1 1 0 100 2h2.586l-6.293 6.293a1 1 0 101.414 1.414L15 6.414V9a1 1 0 102 0V4a1 1 0 00-1-1h-5z"></path>
-                  <path d="M5 5a2 2 0 00-2 2v8a2 2 0 002 2h8a2 2 0 002-2v-3a1 1 0 10-2 0v3H5V7h3a1 1 0 000-2H5z"></path>
-                </svg>
-              </nuxt-link>
-            </div>
-          </div>
-        </div>
-      </div>
-    </section>
     <the-footer/>
   </div>
 </template>
